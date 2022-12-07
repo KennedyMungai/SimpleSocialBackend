28443d4714a93387c3a60fb33c9577f1ac7f2504
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-07 14:46:17 +0300

    Defined the Post class , the my_psos array and the find_post function

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ Author:- Kennedy Mungai
 
 Project:- Simple social media backend
 """
+from pydantic import BaseModel
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
-from pydantic import BaseModel
+
 import mysql.connector
 
 app = FastAPI()
@@ -24,3 +25,32 @@ try:
 except mysql.connector.Error as error:
     print(error)
     exit(1)
+
+
+class Post(BaseModel):
+    """
+    This is the base model for the Post objects
+    Args:
+        BaseModel: The library that makes ot easy to model data
+    """
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "title of post 1", "content": "Content of post 1", "id": 1},
+    {"title": "Favorite foods", "content": "I like pizza", "id": 2}
+]
+
+
+def find_post(_id: int):
+    """
+    This is the function that finds posts
+    Args:
+        _id (int): This is the id of the post in the database
+    """
+    for _p in my_posts:
+        if _p["id"] == _id:
+            return _p
