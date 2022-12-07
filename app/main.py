fdb098044306faeb5fc62bdadc936a400ecae0b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-07 14:40:11 +0300

    Implemented the connection logic

--- app/main.py
+++ app/main.py
@@ -5,8 +5,22 @@ Author:- Kennedy Mungai
 
 Project:- Simple social media backend
 """
-
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
 import mysql.connector
 
 app = FastAPI()
+
+try:
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='xknightmare12873',
+        database='fastapicoursedb'
+    )
+
+    cursor = conn.cursor(dictionary=True)
+except mysql.connector.Error as error:
+    print(error)
+    exit(1)
