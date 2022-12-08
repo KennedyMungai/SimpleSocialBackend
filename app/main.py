a36242f3cbe9a04774b0306e768b622aa5ecbc32
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 10:46:23 +0300

    Moved the dependency code

--- app/main.py
+++ app/main.py
@@ -21,16 +21,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 try:
     conn = mysql.connector.connect(
