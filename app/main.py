725a97768cbac93c8ce23249ea0b8d54cad557d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 12:45:59 +0300

    Defined the response model for the create_user function

--- app/main.py
+++ app/main.py
@@ -169,7 +169,7 @@ def update_post(_id: int, _post: schemas.PostCreate, db: Session = Depends(get_d
     return updated_post
 
 
-@app.post("/user", status_code=status.HTTP_201_CREATED)
+@app.post("/user", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(**user.dict())
     db.add(new_user)
