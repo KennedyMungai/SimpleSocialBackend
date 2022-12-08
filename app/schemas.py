aecc5051a0e17a557d9b56fe2e911da5b6bf834c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 12:39:06 +0300

    Changed the email type to email string

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class PostBase(BaseModel):
@@ -21,5 +21,5 @@ class Post(PostBase):
 
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     password: str
