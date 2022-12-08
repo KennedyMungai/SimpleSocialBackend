6f9ceb045ab243857e40acba1e02c1ea89558105
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 12:42:00 +0300

    Added some create user functionality to teh create_user function

--- app/main.py
+++ app/main.py
@@ -171,4 +171,9 @@ def update_post(_id: int, _post: schemas.PostCreate, db: Session = Depends(get_d
 
 @app.post("/user", status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    pass
+    new_user = models.Post(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return user
