033886833c8b04ab1657bbe8400da0de9c058545
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 10:42:30 +0300

    Added a simple space between the table name definition and the columns

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Boolean, Column, Integer, String
 
 class Post(Base):
     __tablename__ = "posts"
+
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
