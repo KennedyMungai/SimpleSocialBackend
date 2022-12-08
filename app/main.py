9464fc1174e95b7bb2ec100c8e4f79c9d8337bae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 14:11:18 +0300

    Defined the response_model

--- app/main.py
+++ app/main.py
@@ -192,7 +192,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return user
 
 
-@app.get("/users/{id}")
+@app.get("/users/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id).first()
 
