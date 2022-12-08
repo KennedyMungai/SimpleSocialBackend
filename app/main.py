77ecc79039e5eb0130799988770f98871a82f8b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 13:08:43 +0300

    Defoned the get_user function

--- app/main.py
+++ app/main.py
@@ -190,3 +190,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return user
+
+
+@app.get("/users/{id}")
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id).first()
+
+    if not user:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user of id:{id} could not be found")
+
+    return user
