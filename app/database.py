274913f209dcb344c47981edfbc6c7aeee3de3bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-08 10:04:58 +0300

    Passed in the database connection url to yhe create_engine function

--- app/database.py
+++ app/database.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'mysql://root:xknightmare12873@localhost/simple_social_db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
