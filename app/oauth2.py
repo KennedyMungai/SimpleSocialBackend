d373ff246b20b6ec62a8a5795f6200a6f236be9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 12:06:06 +0300

    Initialized the OAuth2PasswordBearer function

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,11 @@
 from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
 from app import schemas
 
+oath2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 # To generate a secret key use 'openssl rand -hex 32'
 SECRET_KEY = "14c497b2746482189dc1b0c1859aa3e4ff9023ffa9c6ae07c2d620693bb5f15f"
