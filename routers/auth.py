362d464498bd5be2161d4c3bb82f0671258a60dc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:26:36 +0300

    Deleted teh dummy return statement and imported oauth2

--- routers/auth.py
+++ routers/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app import models, schemas, utils
+from app import models, schemas, utils, oauth2
 
 
 router = APIRouter(
@@ -22,5 +22,3 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
-
-    return {"token": "example_token"}
