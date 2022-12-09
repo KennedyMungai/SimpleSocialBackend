af533fe4a12e69921424d5b800e8fc3dba9dbc15
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:23:30 +0300

    Implemented the expiry time of the access token(30 minutes from now)

--- app/oauth2.py
+++ app/oauth2.py
@@ -10,3 +10,5 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
