e06f776d3d1dac6c381646632e080bf5dae1a42c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:57:35 +0300

    Added some logic to handle none existent users

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,8 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
+from app import schemas
+
 
 # To generate a secret key use 'openssl rand -hex 32'
 SECRET_KEY = "14c497b2746482189dc1b0c1859aa3e4ff9023ffa9c6ae07c2d620693bb5f15f"
@@ -22,3 +24,8 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
     id = payload.get("user_id")
+
+    if id is None:
+        raise credentials_exception
+
+    token_data = schemas.TokenData
