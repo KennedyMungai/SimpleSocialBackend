e235dab15e8858edc7e3ca650b9975df94681a24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:20:58 +0300

    Defined the create_access_token function and added some barebones functionality

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,3 +5,7 @@ from jose import JWTError, jwt
 SECRET_KEY = "14c497b2746482189dc1b0c1859aa3e4ff9023ffa9c6ae07c2d620693bb5f15f"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
