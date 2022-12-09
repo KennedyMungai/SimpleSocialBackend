f67e31c5965b3ba1bf01fddd6140b642cb2dcfed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 13:09:19 +0300

    Made the get_current_user depend on the database

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
 from app import schemas, database
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -39,7 +40,7 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
