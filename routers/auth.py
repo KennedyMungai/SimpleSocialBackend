9fe39d6d5d4cff3d9a82cb66a19cefa05056efd3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:32:05 +0300

    Imported the OAuth2 password request form

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database import get_db
