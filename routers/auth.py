ff4fa2913a151c2673b06bb662d1c7921a9b2d4d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:29:11 +0300

    Returned the access_token and the token type(bearer)

--- routers/auth.py
+++ routers/auth.py
@@ -22,3 +22,7 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
