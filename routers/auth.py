2eee59722a088f0208d2b4f42a801afbb9afedc7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:00:46 +0300

    Wrote a condition to handle wrong email input

--- routers/auth.py
+++ routers/auth.py
@@ -14,3 +14,7 @@ router = APIRouter(
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(
         models.User.email == user_credentials.email).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
