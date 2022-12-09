40459717204aa34d3252b50fe850a1e59d25ea0c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-09 11:19:43 +0300

    Added variables for the algorithm and the access_token_expire_minutes

--- app/oauth2.py
+++ app/oauth2.py
@@ -3,3 +3,5 @@ from jose import JWTError, jwt
 
 # To generate a secret key use 'openssl rand -hex 32'
 SECRET_KEY = "14c497b2746482189dc1b0c1859aa3e4ff9023ffa9c6ae07c2d620693bb5f15f"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
