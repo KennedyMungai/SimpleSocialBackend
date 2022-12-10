57b66ae113d8d007adb4b2fc8a81edc0b646a214
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-10 12:03:16 +0300

    Moved all the user code above

--- app/schemas.py
+++ app/schemas.py
@@ -13,15 +13,6 @@ class PostCreate(PostBase):
     pass
 
 
-class Post(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -39,6 +30,16 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
 
 
 class Token(BaseModel):
