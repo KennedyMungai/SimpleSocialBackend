75815df9ae7bc3b38fb07687fd4f14fc69a03e9c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-10 12:29:48 +0300

    Added an import for mysql connector

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import mysql.connector
 
 
 SQLALCHEMY_DATABASE_URL = 'mysql://root:xknightmare12873@localhost/simple_social_db'
