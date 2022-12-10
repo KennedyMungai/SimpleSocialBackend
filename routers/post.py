75a2013303c6256b30b7d152597eef8e39d73eca
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-10 12:08:00 +0300

    Added the limit parameter to the get_posts function

--- routers/post.py
+++ routers/post.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int):
     """
         This is a simple function that retrieves all the posts
     """
