acf724afd1e0bfb3f4a166d26a4498f3da2fbee0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-10 11:57:06 +0300

    Removed the filtering from all the posts

--- routers/post.py
+++ routers/post.py
@@ -21,8 +21,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # posts = cursor.execute('SELECT * FROM posts')
     # print(posts)
 
-    posts = db.query(models.Post).filter(
-        models.Post.owner_id == current_user.id).all()
+    posts = db.query(models.Post).all()
     return posts
 
 
