6bcf8878945b59dc8e9e547683d0df1cf904b297
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-10 10:14:56 +0300

    Assigned the id primary key ti the foreign key

--- routers/post.py
+++ routers/post.py
@@ -34,7 +34,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     #                           (post.title, post.content, post.is_published))
     # conn.commit()
 
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id,  ** post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
