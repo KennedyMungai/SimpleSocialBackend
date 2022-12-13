9598e01903e9068bb738678446c6cd5b1976c396
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-13 15:19:32 +0300

    Imported Session from sqlalchemy

--- routers/vote.py
+++ routers/vote.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from app import schemas, database, models, oauth2
+from sqlalchemy.orm import Session
 
 
 router = APIRouter(
