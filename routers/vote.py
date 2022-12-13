77aa1357c2c6d69b6cc75ceb625091d3d29e36d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-13 15:20:36 +0300

    SOme simple logic for the voting system

--- routers/vote.py
+++ routers/vote.py
@@ -11,4 +11,7 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
-    pass
+    if (vote.dir == 1):
+        pass
+    else:
+        pass
