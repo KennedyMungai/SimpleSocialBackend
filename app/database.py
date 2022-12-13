33bad24b5c802c5cd388a6f0d8f6639a8b9aff8b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-13 14:17:52 +0300

    Added an import for the config file

--- app/database.py
+++ app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import mysql.connector
+import config
 
 
-SQLALCHEMY_DATABASE_URL = 'mysql://root:xknightmare12873@localhost/simple_social_db'
+SQLALCHEMY_DATABASE_URL = f'mysql://root:xknightmare12873@localhost/simple_social_db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
