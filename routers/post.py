5ad38386324b17c4f8c9cdbaac520ecf8f9f6f60
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-14 13:08:31 +0300

    Renamed the variablke to posts and returned ot

--- routers/post.py
+++ routers/post.py
@@ -31,13 +31,13 @@ def get_posts(
     # posts = cursor.execute('SELECT * FROM posts')
     # print(posts)
 
-    posts = db.query(models.Post).filter(
-        models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(
+    #     models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("Votes")).join(models.Vote, models.Vote.post_id ==
-                                                                                         models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("Votes")).join(models.Vote, models.Vote.post_id ==
+                                                                                       models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-    return results
+    return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
