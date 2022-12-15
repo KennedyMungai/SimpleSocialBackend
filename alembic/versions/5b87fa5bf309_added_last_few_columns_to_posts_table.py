f9e65800f3889c3be8a551c6a366edfe7bd9bee3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-15 15:46:16 +0300

    Generated new revisions file

---
+++ alembic/versions/5b87fa5bf309_added_last_few_columns_to_posts_table.py
@@ -0,0 +1,24 @@
+"""Added last few columns to posts table
+
+Revision ID: 5b87fa5bf309
+Revises: 78f13bc28603
+Create Date: 2022-12-15 15:45:55.694325
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5b87fa5bf309'
+down_revision = '78f13bc28603'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
