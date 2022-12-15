b4c4f70f4af19aa1814d71ecb5574d6cab6a9830
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-15 15:01:18 +0300

    A revision for adding a user table

---
+++ alembic/versions/fa67f70a6946_add_a_user_table.py
@@ -0,0 +1,24 @@
+"""Add a user table
+
+Revision ID: fa67f70a6946
+Revises: 032383e11726
+Create Date: 2022-12-15 15:00:53.747403
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa67f70a6946'
+down_revision = '032383e11726'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
