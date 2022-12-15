4a727c88e5a6795d4e896024aec56580699cb040
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-15 14:31:17 +0300

    First vesion control of database tables

---
+++ alembic/versions/6efba9468fdf_create_posts_table.py
@@ -0,0 +1,24 @@
+"""Create posts table
+
+Revision ID: 6efba9468fdf
+Revises: 
+Create Date: 2022-12-15 14:30:20.006975
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6efba9468fdf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
